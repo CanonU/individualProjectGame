3b63bae486035ed6ab66e3ca75bcf765a4d9eee5
CanonU <142544235+CanonU@users.noreply.github.com>
2024-03-21 10:22:59 -0600

    update target.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 #Canon Unguren
 import pygame
+import random
+from target import Target
 
 timeScore = 0
+minuteScore =0
 targetScore = 0
-equationScore = 0
-arrowScore = 0
-
-
 play = False
 
 #Initialize timer variables
 current_time = 0
-timer_interval = 100
+timer_interval = 75
 
+
+def targetfunc():
+    global targetScore
+    targetScore += 1
+    targetButton.x = random.randint(50,450)
+
+    
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
@@ -39,10 +45,38 @@ while not play:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             play = True
+
+
+
+target_image = pygame.image.load("images/target.png")
+#Main Game loop
 while play:
+    screen.fill((255, 255, 255))
+    targetButton = Target(100, 300, pebblebutton1_image, pebbleclick)
+    if timeScore >= 10:
+        timeRender = font.render(f"Time: {minuteScore}:{timeScore}", True, (0, 0, 0))
+    if timeScore <= 10:
+        timeRender = font.render(f"Time: {minuteScore}:0{timeScore}", True, (0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+
+     # Check the elapsed time for the timer
+    current_time += clock.get_rawtime()
+    clock.tick()
+    
+    # Update score every second
+    if current_time >= timer_interval:
+        timeScore += 1
+        if timeScore >= 60:
+            timeScore -= 60
+            minuteScore += 1
+        print(f"TimeScore: {minuteScore}:{timeScore}")
+        current_time = 0
+
+        
+    screen.blit(timeRender,(10,10))
+
     pygame.display.flip()
     clock.tick(60)
     pygame.display.update()
--- src/target.py
+++ src/target.py
@@ -1,1 +1,18 @@
+#Canon U
 
+import pygame
+import sys
+
+class Target:
+    def __init__(self, x, y, image, function):
+        self.rect = image.get_rect(topleft=(x, y))
+        self.image = image
+        self.function = function
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.function()

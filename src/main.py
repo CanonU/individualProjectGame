1feb78c1ae68f29d1b20f44cc1689872e5caa4ff
CanonU <142544235+CanonU@users.noreply.github.com>
2024-03-29 11:01:17 -0600

    update circle.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@
 import pygame
 import random
 from target import Target
-
+from circle import Circle
+mousex, mousey = pygame.mouse.get_pos()
 timeScore = 0
 minuteScore =0
 targetScore = 0
 play = False
-
 #Initialize timer variables
 current_time = 0
 timer_interval = 75
-
+circle_count = 50
+circles = [Circle() for _ in range(circle_count)]
+size = 10
 
 def targetfunc():
     global targetScore
@@ -51,6 +53,7 @@ while not play:
 target_image = pygame.image.load("images/target.png")
 #Main Game loop
 while play:
+    #pygame.mouse.set_visible(False)
     screen.fill((255, 255, 255))
     targetButton = Target(100, 300, target_image, targetfunc)
     if timeScore >= 10:
@@ -61,7 +64,19 @@ while play:
         if event.type == pygame.QUIT:
             play = False
         targetButton.handle_event(event)
+        if event.type == pygame.MOUSEMOTION:
+            mousex, mousey = pygame.mouse.get_pos()
+    pygame.draw.circle(screen,(0,0,0),(mousex,mousey), size)
 
+    for circle in circles:
+        circle.move()
+        circle.draw(screen)
+
+        mousex, mousey = pygame.mouse.get_pos()
+        #if circle.x +circle.radius > mousex-size+2 and circle.x -circle.radius < mousex+size-2 and circle.y +circle.radius > mousey-size+2 and circle.y -circle.radius < mousey+size-2:
+        distance = ((circle.x-(mousex-size))**2 + (circle.y - (mousey-size))**2)**0.5
+        if distance <= circle.radius:
+            play = False
      # Check the elapsed time for the timer
     current_time += clock.get_rawtime()
     clock.tick()
@@ -73,11 +88,10 @@ while play:
             timeScore -= 60
             minuteScore += 1
         print(f"TimeScore: {minuteScore}:{timeScore}")
+        print(f"DodgeScore: {circle.dodgescore}")
         current_time = 0
 
-        
     screen.blit(timeRender,(10,10))
-
     pygame.display.flip()
     clock.tick(60)
     pygame.display.update()

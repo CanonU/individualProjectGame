7a43f187aa1747b485c984b72e589d745ca9fc0c
CanonU <142544235+CanonU@users.noreply.github.com>
2024-04-25 12:24:17 -0600

    eee

--- src/experience.py
+++ src/experience.py
@@ -2,20 +2,14 @@ import pygame
 import random
 
 pygame.init()
-
 class Orb:
-    def __init__(self):
-        self.image = pygame.image.load("clock.png")
-        self.image = pygame.transform.scale(self.image, (10, 10))
-        self.radius = 10
 
-        # Randomize initial position
-        self.x = random.randint(0, pygame.display.Info().current_w - self.radius)
-        self.y = random.randint(0, pygame.display.Info().current_h - self.radius)
+    def __init__(self):
+        self.radius = 10
+        self.color = (random.randint(0, 200), random.randint(0, 200),random.randint(0, 200))
+        self.x = pygame.display.Info().current_w + self.radius
+        self.x = random.randint(self.radius, pygame.display.Info().current_w - self.radius)
+        self.y = random.randint(self.radius, pygame.display.Info().current_h - self.radius)
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
-    def update_size(self):
-        # Change the size of the image
-        new_size = 10
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
